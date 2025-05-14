84f064fa82f146b30633caf6714e221ac4d9a961
luizjuniorr <142500782+luizjuniorr@users.noreply.github.com>
2025-05-14 07:28:32 -0300

    Atualizar o Calculadora.jsx

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -3,36 +3,57 @@ import { useState } from 'react'
 export function Calculadora() {
     const [salario, setSalario] = useState(0)
     const [resultado, setResultado] = useState(0)
+    const [aliquota, setAliquota] = useState(0)
+    const [deducao, setDeducao] = useState(0)
 
     function calcular() {
         let desconto = 0
+        let a = 0
+        let d = 0
 
         if (salario <= 1518) {
-        desconto = salario * 0.075
-        } else if (salario >= 1518.01 && salario <= 2798.88) {
-        desconto = (salario * 0.09) - 20.77
-        } else if (salario >= 2798.89 && salario <= 4190.83) {
-        desconto = (salario * 0.12) - 106.50
-        } else if (salario >= 4190.84 && salario <= 8157.42) {
-        desconto = (salario * 0.14) - 190.40
+            a = 0.075
+            d = 0
+        } else if (salario <= 2798.88) {
+            a = 0.09
+            d = 20.77
+        } else if (salario <= 4190.83) {
+            a = 0.12
+            d = 106.50
+        } else if (salario <= 8157.42) {
+            a = 0.14
+            d = 190.40
         }
 
+        desconto = (salario * a) - d
         setResultado(desconto)
+        setAliquota(a)
+        setDeducao(d)
     }
 
-    return(
+    return (
         <div className='text-center'>
             <div className="flex items-center justify-center flex-col gap-2">
                 <h2 className="uppercase">Insira o salário:</h2>
-                <input type="number" className="text-center border-1 border-black rounded-2xl" value={salario} onChange={(e) => setSalario(Number(e.target.value))}/>
-                <button className="bg-orange-500 w-full rounded-3xl text-white uppercase" onClick={calcular}> Calcula </button>
+                <input
+                    type="number"
+                    className="text-center border-1 border-black rounded-2xl"
+                    value={salario}
+                    onChange={(e) => setSalario(Number(e.target.value))}
+                />
+                <button
+                    className="bg-orange-500 w-full rounded-3xl text-white uppercase"
+                    onClick={calcular}
+                >
+                    Calcula
+                </button>
             </div>
 
             <div>
-                <p>Desconto: R$ {resultado.toFixed(2)}</p>
+                <p>
+                    Desconto: R$ {salario} * {aliquota} - {deducao} = R$ {resultado.toFixed(2)}
+                </p>
             </div>
-
-            
         </div>
     )
 }
