69e27804b2ed5bd117aaffb27cbf5fee2d95cb82
luizjuniorr <142500782+luizjuniorr@users.noreply.github.com>
2025-05-14 07:35:17 -0300

    Atualizar o Calculadora.jsx

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,31 +1,32 @@
 import { useState } from 'react'
 
 export function Calculadora() {
-    const [salario, setSalario] = useState(0)
+    const [salario, setSalario] = useState('')
     const [resultado, setResultado] = useState(0)
     const [aliquota, setAliquota] = useState(0)
     const [deducao, setDeducao] = useState(0)
 
     function calcular() {
+        const valorNumerico = Number(salario.replace(',', '.')) // aceita vírgula ou ponto
         let desconto = 0
         let a = 0
         let d = 0
 
-        if (salario <= 1518) {
+        if (valorNumerico <= 1518) {
             a = 0.075
             d = 0
-        } else if (salario <= 2798.88) {
+        } else if (valorNumerico <= 2798.88) {
             a = 0.09
             d = 20.77
-        } else if (salario <= 4190.83) {
+        } else if (valorNumerico <= 4190.83) {
             a = 0.12
             d = 106.50
-        } else if (salario <= 8157.42) {
+        } else if (valorNumerico <= 8157.42) {
             a = 0.14
             d = 190.40
         }
 
-        desconto = (salario * a) - d
+        desconto = (valorNumerico * a) - d
         setResultado(desconto)
         setAliquota(a)
         setDeducao(d)
@@ -36,10 +37,11 @@ export function Calculadora() {
             <div className="flex items-center justify-center flex-col gap-2">
                 <h2 className="uppercase">Insira o salário:</h2>
                 <input
-                    type="number"
+                    type="text"
                     className="text-center border-1 border-black rounded-2xl"
                     value={salario}
-                    onChange={(e) => setSalario(Number(e.target.value))}
+                    onChange={(e) => setSalario(e.target.value)}
+                    placeholder="Ex: 2500,50"
                 />
                 <button
                     className="bg-orange-500 w-full rounded-3xl text-white uppercase"
